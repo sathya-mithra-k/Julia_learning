2c42514d14679cdf1aee81fccedf576413d7c6b4
Sathya Mithra K <150320451+sathya-mithra-k@users.noreply.github.com>
2024-09-16 15:48:45 +0530

    Add files via upload

---
+++ Variables.jl
@@ -0,0 +1,8 @@
+# Variables are box container containing data
+student_name = "Ram"
+roll_no = 15
+marks_math = 9.5
+# Like other programming languages we do not need to specify the type of Variables in julia, it can infer itself.
+
+#= for multi line command =#
+print(student_name)
---
+++ types1.jl
@@ -0,0 +1,44 @@
+# integerr
+typeof(3)
+
+#float
+typeof(3.14)
+
+typeof(NaN)
+
+#rational number
+typeof(5//2)
+
+pi
+# pi is an built in variable in julia unlike python
+
+sqrt(4)
+cbrt(8)
+convert(Int64,3.0)
+iseven(5)
+isodd(7)
+rem(5,2)
+round(3.142568, digits = 2)
+
+# = means variable operator
+# == equality test
+# === identity test
+
+a = 3
+b = 3.0
+a == b
+a === b
+
+typeof('a')
+δ
+# / this operator always gives a floating point
+# div() gives integer 
+div(5,2)
+
+# \ approx
+0.1 + 0.2 ≈ 0.3
+
+❄
+🐕
+
+Char(65) # gives ASCII code
